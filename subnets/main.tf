e37bd1abcb26fb736ce0c0675f14ed38ed253c3d
kr734 <7981kri@gmail.com>
2024-08-14 22:50:47 +0530

    Terraform

--- subnets/main.tf
+++ subnets/main.tf
@@ -1,8 +1,8 @@
 resource "aws_subnet" "main" {
   for_each = var.subnets
   vpc_id     = var.vpc_id
-  cidr = each.value["cidr"]
-  az = each.value["az"]
+  cidr_block = each.value["cidr"]
+  availability_zone = each.value["az"]
 
   tags = {
     Name = ""
