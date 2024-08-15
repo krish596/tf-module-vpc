c06a66e60921ab9dd0ea1040d28a321bb8ae6b08
Chimtamreddy <kr7348202@gmail.com>
2024-08-15 14:25:20 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -29,6 +29,10 @@ resource "aws_eip" "ngw" {
   domain = "vpc"
 }
 
-
+resource "aws_nat_gateway" "ngw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "subnet_ids", null)
+  allocation_id = lookup(lookup(aws_eip.ngw, each.key, null), "id", null)
+  subnet_id     = each.value["id"]
+}
 
 
