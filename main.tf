0f6efb2c3b593a874341ec59f2c67cda0bd207c6
Chimtamreddy <kr7348202@gmail.com>
2024-08-15 19:24:56 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -62,34 +62,4 @@ resource "aws_route" "default-vpc-peer-entry" {
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
 
-resource "aws_instance" "main" {
-  ami           = "ami-0b4f379183e5706b9"
-  instance_type = "t3.small"
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_id = local.app_subnet_ids[0]
-}
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.main.id
 
-  tags = {
-    Name = "allow_tls"
-  }
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-}
