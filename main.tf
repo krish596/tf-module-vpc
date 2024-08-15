01b218cb1a399cd2af3f98bfdf3695390c970361
Chimtamreddy <kr7348202@gmail.com>
2024-08-15 18:50:29 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -88,9 +88,9 @@ resource "aws_security_group" "allow_tls" {
   }
 
   egress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
