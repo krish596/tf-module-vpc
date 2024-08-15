e709a6cd8e51790f7e38b7cc1f713fdd50a4968f
Chimtamreddy <kr7348202@gmail.com>
2024-08-15 15:29:38 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -67,7 +67,7 @@ resource "aws_instance" "instance" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
-  subnet_ids = length(private_subnet_ids[0])
+  subnet_id = length(private_subnet_ids[0])
 }
 
 resource "aws_security_group" "allow_tls" {
