aceb4fa6bb8208fbeec05602d965f60a6a3dcc47
Chimtamreddy <kr7348202@gmail.com>
2024-08-15 15:21:10 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -49,9 +49,16 @@ resource "aws_vpc_peering_connection" "peering" {
   auto_accept = true
 }
 
-resource "aws_route" "ngw" {
+resource "aws_route" "peer" {
   count = length(local.private_route_table_ids)
   route_table_id            = element(local.private_route_table_ids, count.index)
   destination_cidr_block    = var.default_vpc_cidr
-  nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
+  vpc_peering_connection = aws_vpc_peering_connection.peering.id
+}
+
+resource "aws_route" "default-vpc-peer-entry" {
+
+  route_table_id            = element(local.private_route_table_ids, count.index)
+  destination_cidr_block    = var.default_vpc_route_table_id
+  vpc_peering_connection = aws_vpc_peering_connection.peering.id
 }
