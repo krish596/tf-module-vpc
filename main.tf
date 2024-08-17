01f96f90f6e5f8d4cf77f14a95ae96d587a12058
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 20:55:15 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 resource "aws_vpc" "main" {
-  for_each = var.subnets
   cidr_block = var.cidr
 }
 
 module "subnets" {
   source = "./subnets"
-  for_each = module.subnets
+  for_each = var.subnets
   subnets = each.value
   vpc_id = aws_vpc.main.id
 }
