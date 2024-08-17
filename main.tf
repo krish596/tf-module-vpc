6334554a2de0ad4e3a6094555dc74e397bf34d09
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 22:13:39 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -39,3 +39,9 @@ resource "aws_route" "ngw" {
   destination_cidr_block    = "0.0.0.0/0"
   nat_gateway_id = element(aws_nat_gateway.ngw.*.id, count.index)
 }
+
+resource "aws_vpc_peering_connection" "peering" {
+
+  peer_vpc_id   = aws_vpc.main.id
+  vpc_id        = var.default_vpc_id
+}
