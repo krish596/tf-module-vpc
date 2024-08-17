04de0c8a198e909663bc508653bb8dce98880bba
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 21:19:17 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -13,3 +13,12 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
 }
+output "vpc" {
+  value = module.subnets
+}
+# resource "aws_route" "igw" {
+#   for_each = var.
+#   route_table_id            = aws_route_table.testing.id
+#   destination_cidr_block    = "10.0.1.0/22"
+#   vpc_peering_connection_id = "pcx-45ff3dc1"
+# }
---
+++ subnets/outputs.tf
@@ -0,0 +1,7 @@
+output "subnets" {
+  value = aws_subnet.main
+}
+
+output "route_table" {
+  value = aws_route_table.main
+}
