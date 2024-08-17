fd3e17961f64f0bb588736a0297ce8b6a7ee9c87
Chimtamreddy <kr7348202@gmail.com>
2024-08-17 21:28:46 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -13,12 +13,10 @@ resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
 
 }
-output "vpc" {
-  value = module.subnets
+
+resource "aws_route" "igw" {
+  for_each = lookup(lookup(module.subnets, "public", null), "route_table_ids", null)
+  route_table_id            = each.value["id"]
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
 }
-# resource "aws_route" "igw" {
-#   for_each = var.
-#   route_table_id            = aws_route_table.testing.id
-#   destination_cidr_block    = "10.0.1.0/22"
-#   vpc_peering_connection_id = "pcx-45ff3dc1"
-# }
